665bff58124081d42b87b522abff0b222cf0de59
abdelrahman <abdelrahmanosama372@gmail.com>
2024-04-25 12:49:51 +0200

    add diffbot controllers

---
+++ diffbot_controllers/launch/controllers.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager","/controller_manager"]
+    )
+
+    velocity_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["simple_velocity_controller",
+                   "--controller-manager","/controller_manager"]
+    )
+
+    return LaunchDescription(
+        [
+            joint_state_broadcaster_spawner,
+            velocity_controller_spawner
+        ]
+    )
